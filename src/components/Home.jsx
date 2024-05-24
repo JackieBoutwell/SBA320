a8773296c97df6617dad95694a7f8515986738f5
Jackie Boutwell <boutwelljackie983@gmail.com>
2024-05-23 19:48:46 -0600

    Met all requirements and added useReducer for the delete

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,91 +1,105 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useReducer } from 'react';
 import Plantsinfo from './component2/Plantsinfo';
 
 const Home = () => {
     // // let name ='jackie';
     //const [apiData, setData] = useState({});
     
-    const [plants, setPlants] = useState([]);
-  
+    //const [plants, setPlants] = useState([]);
+
+    const reducer = (state, action) => {
+        switch (action.type) {
+            case 'delete_plant':
+                return { plants: state.plants.filter(plant => plant.id !== action.id) };
+            case 'add_plants':
+                return { plants: action.plants }
+        }
+    }
+
+    const [state, dispatch] = useReducer(reducer, {});
+
     const getPlants = async () => {
         // Use AJAX to make a request to an external data source like OMDBapi, and insert some of the data retrieved into the DOM.
-        //const response = await fetch(
-            //`https://perenual.com/api/species-list?key=sk-wgYH664bba6e628725587`
+        const response = await fetch(
+            `https://perenual.com/api/species-list?key=sk-wgYH664bba6e628725587`
             //`https://perenual.com/api/species/details/${searchTerm}?key=sk-wgYH664bba6e628725587`
-        //);
-        //const data = await response.json();
-        const data = {
-            "data": [
-                {
-                    "id": 1,
-                    "common_name": "European Silver Fir",
-                    "scientific_name": [
-                        "Abies alba"
-                    ],
-                    "other_name": [
-                        "Common Silver Fir"
-                    ],
-                    "cycle": "Perennial",
-                    "watering": "Frequent",
-                    "sunlight": [],
-                    "default_image": {
-                        "image_id": 9,
-                        "license": 5,
-                        "license_name": "Attribution-ShareAlike License",
-                        "license_url": "https://creativecommons.org/licenses/by-sa/2.0/",
-                        "original_url": "https://perenual.com/storage/species_image/2_abies_alba_pyramidalis/og/49255769768_df55596553_b.jpg",
-                        "regular_url": "https://perenual.com/storage/species_image/2_abies_alba_pyramidalis/regular/49255769768_df55596553_b.jpg",
-                        "medium_url": "https://perenual.com/storage/species_image/2_abies_alba_pyramidalis/medium/49255769768_df55596553_b.jpg",
-                        "small_url": "https://perenual.com/storage/species_image/2_abies_alba_pyramidalis/small/49255769768_df55596553_b.jpg",
-                        "thumbnail": "https://perenual.com/storage/species_image/2_abies_alba_pyramidalis/thumbnail/49255769768_df55596553_b.jpg"
-                    }
-                },
-                {
-                    "id": 2,
-                    "common_name": "Pyramidalis Silver Fir",
-                    "scientific_name": [
-                        "Abies alba 'Pyramidalis'"
-                    ],
-                    "cycle": "Perennial",
-                    "watering": "Frequent",
-                    "sunlight": [],
-                    "other_name": null,
-                    "default_image": {
-                        "image_id": 9,
-                        "license": 5,
-                        "license_name": "Attribution-ShareAlike License",
-                        "license_url": "https://creativecommons.org/licenses/by-sa/2.0/",
-                        "original_url": "https://perenual.com/storage/species_image/2_abies_alba_pyramidalis/og/49255769768_df55596553_b.jpg",
-                        "regular_url": "https://perenual.com/storage/species_image/2_abies_alba_pyramidalis/regular/49255769768_df55596553_b.jpg",
-                        "medium_url": "https://perenual.com/storage/species_image/2_abies_alba_pyramidalis/medium/49255769768_df55596553_b.jpg",
-                        "small_url": "https://perenual.com/storage/species_image/2_abies_alba_pyramidalis/small/49255769768_df55596553_b.jpg",
-                        "thumbnail": "https://perenual.com/storage/species_image/2_abies_alba_pyramidalis/thumbnail/49255769768_df55596553_b.jpg"
-                    }
-                }
-            ],
-            "to": 30,
-            "per_page": 30,
-            "current_page": 1,
-            "from": 1,
-            "last_page": 405,
-            "total": 10104
-        };
-        setPlants(data.data);
+        );
+        const data = await response.json();
+        //console.log(data.data.default_image.small_url);
+        // const data = {
+        //     "data": [
+        //         {
+        //             "id": 1,
+        //             "common_name": "European Silver Fir",
+        //             "scientific_name": [
+        //                 "Abies alba"
+        //             ],
+        //             "other_name": [
+        //                 "Common Silver Fir"
+        //             ],
+        //             "cycle": "Perennial",
+        //             "watering": "Frequent",
+        //             "sunlight": [],
+        //             "default_image": {
+        //                 "image_id": 9,
+        //                 "license": 5,
+        //                 "license_name": "Attribution-ShareAlike License",
+        //                 "license_url": "https://creativecommons.org/licenses/by-sa/2.0/",
+        //                 "original_url": "https://perenual.com/storage/species_image/2_abies_alba_pyramidalis/og/49255769768_df55596553_b.jpg",
+        //                 "regular_url": "https://perenual.com/storage/species_image/2_abies_alba_pyramidalis/regular/49255769768_df55596553_b.jpg",
+        //                 "medium_url": "https://perenual.com/storage/species_image/2_abies_alba_pyramidalis/medium/49255769768_df55596553_b.jpg",
+        //                 "small_url": "https://perenual.com/storage/species_image/2_abies_alba_pyramidalis/small/49255769768_df55596553_b.jpg",
+        //                 "thumbnail": "https://perenual.com/storage/species_image/2_abies_alba_pyramidalis/thumbnail/49255769768_df55596553_b.jpg"
+        //             }
+        //         },
+        //         {
+        //             "id": 2,
+        //             "common_name": "Pyramidalis Silver Fir",
+        //             "scientific_name": [
+        //                 "Abies alba 'Pyramidalis'"
+        //             ],
+        //             "cycle": "Perennial",
+        //             "watering": "Frequent",
+        //             "sunlight": [],
+        //             "other_name": null,
+        //             "default_image": {
+        //                 "image_id": 9,
+        //                 "license": 5,
+        //                 "license_name": "Attribution-ShareAlike License",
+        //                 "license_url": "https://creativecommons.org/licenses/by-sa/2.0/",
+        //                 "original_url": "https://perenual.com/storage/species_image/2_abies_alba_pyramidalis/og/49255769768_df55596553_b.jpg",
+        //                 "regular_url": "https://perenual.com/storage/species_image/2_abies_alba_pyramidalis/regular/49255769768_df55596553_b.jpg",
+        //                 "medium_url": "https://perenual.com/storage/species_image/2_abies_alba_pyramidalis/medium/49255769768_df55596553_b.jpg",
+        //                 "small_url": "https://perenual.com/storage/species_image/2_abies_alba_pyramidalis/small/49255769768_df55596553_b.jpg",
+        //                 "thumbnail": "https://perenual.com/storage/species_image/2_abies_alba_pyramidalis/thumbnail/49255769768_df55596553_b.jpg"
+        //             }
+        //         }
+        //     ],
+        //     "to": 30,
+        //     "per_page": 30,
+        //     "current_page": 1,
+        //     "from": 1,
+        //     "last_page": 405,
+        //     "total": 10104
+        // };
+        dispatch({
+            type: 'add_plants',
+            plants: data.data
+        })
+        //setPlants(data.data);
     };
 
     useEffect(() => {
         getPlants();
     }, []);
 
-
-          const handleDelete = (id) => {
-            const newPlants = plants.filter(plant => blog.id !== id);
-            setPlants (newPlants)
-        }
-        
-
-    const handleClick = () => {
-        setName('jackie');
+    const handleDelete = (id) => {
+        dispatch({
+            type: 'delete_plant',
+            id: id
+        })
+        // const newPlants = plants.filter(plant => plant.id !== id);
+        // setPlants(newPlants);
     }
     // const handClickAgain = (name) => {
     // console.log('hello' + name);
@@ -93,25 +107,7 @@ const Home = () => {
     
     return ( 
         <div className="home">
-            {plants.map((data) => (
-                <div className="plant-preview" key={data.id}>
-                    <h2> {data.common_name} </h2>
-                    <h2> {data.scientific_name[0]}</h2>
-
-                    {data.sunlight.length > 0 && <p>Sunlight Guide  </p>}
-                    
-                    <p>Water Guide {data.watering}</p>
-
-                    <img src={data.default_image.small_url}></img>
-                </div>
-            ))}
-
-            <Plantsinfo plants={plants} handleDelete={handleDelete} />
-            
-            <h2>Homepage</h2>
-
-            <button onClick={handleClick}> ClickMe</button>
-
+            <Plantsinfo plants={state.plants} handleDelete={handleDelete} />
             {/* <button onClick={() => {
                 console.log ('test two')
             }}> ClickMeAgain</button> */}
--- src/components/component2/Plantsinfo.jsx
+++ src/components/component2/Plantsinfo.jsx
@@ -1,21 +1,23 @@
-const Plantsinfo = (props, handleDelete) => { 
+import '../../index.css';
+
+const Plantsinfo = ({ plants, handleDelete }) => { 
     // Adding props and delete button here
-    const plants = props.plants;
- 
+    //const plants = props.plants;
     return(
         <div className="Plants-info">
-            {plants.map((data) => (
+            {plants && plants.map((data) => (
+                
                 <div className="plant-preview" key={data.id}>
                     <h2> {data.common_name} </h2>
                     <h2> {data.scientific_name[0]}</h2>
                  {/* Shows Sunlight Guide if it has info if not will not show the user. */}
-                    {data.sunlight.length > 0 && <p>Sunlight Guide  </p>}
+                    {data.sunlight.length > 0 && <p>Sunlight Guide  {data.sunlight[0]}</p>}
                     
                     <p>Water Guide {data.watering}</p>
 
-                    <img src={data.default_image.small_url}></img>
+                    {data.default_image && <img src={data.default_image.small_url}></img>}
                     {/* Creating a Delete button */}
-                    <button onClick={() => handleDelete(plants.id)}> Delete plant</button>
+                    <button onClick={() => handleDelete(data.id)}> Delete plant</button>
                 </div>
             ))}
         </div>
